8b76fb92147b44f405ac51d9dc4d24e05697e83a
Jacob Kilgore <jake.h.kilgore@gmail.com>
2024-03-06 18:37:56 -0600

    created code for preventing arm damage
    we coded for threshold by  creating mathematical formulas. we did this because we did not want the arm to move to far and resulting in damage

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -8,33 +8,45 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 
 public class Arm extends SubsystemBase {
   CANSparkMax leftMotor ;
   CANSparkMax rightMotor; 
-  private final AbsoluteEncoder encoder; 
+  private final RelativeEncoder encoder; 
+  private final double min = -1;
+  private final double max = -1; //TODO: figure out what this value actually is
+  private final double threshold = -1;
+  private final boolean constrain = false;
   
   /** Creates a new Climber. */
   public Arm() {
     leftMotor = new CANSparkMax(ArmConstants.LEFT_MOTOR_CHANNEL, MotorType.kBrushless);
     rightMotor = new CANSparkMax(ArmConstants.RIGHT_MOTOR_CHANNEL, MotorType.kBrushless);
-    leftMotor.setInverted(true);
-    encoder = leftMotor.getAbsoluteEncoder(Type.kDutyCycle);
-    encoder.setInverted(true);
+    rightMotor.setInverted(true);
+    encoder = leftMotor.getEncoder();
   }
+
   public void run (double speed){
+    double current = encoder.getPosition();
+    if (constrain ){
+      if (current > max - threshold){
+        speed *= (max - current)/threshold;
+      }
+      if (current < min + threshold){
+        speed *= (current - min)/threshold;
+      }
+    }
     leftMotor.set(speed);
     rightMotor.set(speed);
   }
-  public void run (double leftspeed, double rightspeed){
-    leftMotor.set(leftspeed);
-    rightMotor.set(rightspeed);
-  }
+  
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Encoder", encoder.getPosition());
   }
 }
