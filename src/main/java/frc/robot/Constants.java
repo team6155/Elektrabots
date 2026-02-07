ddb4d00eb6e89ef1b675e68b3aedc79f1ca77dc8
Jacob Kilgore <jake.h.kilgore@gmail.com>
2026-02-06 21:16:51 -0600

    KitBot code setup
    We'll be using the FRC kitbot this year, so this is the kitbot code provided from the website.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,135 +4,58 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
-public final class Constants extends Object {
-    // Computer USB ports
+public final class Constants {
+  public static final class DriveConstants {
+    // Motor controller IDs for drivetrain motors
+    public static final int LEFT_LEADER_ID = 1;
+    public static final int LEFT_FOLLOWER_ID = 2;
+    public static final int RIGHT_LEADER_ID = 3;
+    public static final int RIGHT_FOLLOWER_ID = 4;
+
+    // Current limit for drivetrain motors. 60A is a reasonable maximum to reduce
+    // likelihood of tripping breakers or damaging CIM motors
+    public static final int DRIVE_MOTOR_CURRENT_LIMIT = 60;
+  }
+
+  public static final class FuelConstants {
+    // Motor controller IDs for Fuel Mechanism motors
+    public static final int FEEDER_MOTOR_ID = 6;
+    public static final int INTAKE_LAUNCHER_MOTOR_ID = 5;
+
+    // Current limit and nominal voltage for fuel mechanism motors.
+    public static final int FEEDER_MOTOR_CURRENT_LIMIT = 60;
+    public static final int LAUNCHER_MOTOR_CURRENT_LIMIT = 60;
+
+    // Voltage values for various fuel operations. These values may need to be tuned
+    // based on exact robot construction.
+    // See the Software Guide for tuning information
+    public static final double INTAKING_FEEDER_VOLTAGE = -12;
+    public static final double INTAKING_INTAKE_VOLTAGE = 10;
+    public static final double LAUNCHING_FEEDER_VOLTAGE = 9;
+    public static final double LAUNCHING_LAUNCHER_VOLTAGE = 10.6;
+    public static final double SPIN_UP_FEEDER_VOLTAGE = -6;
+    public static final double SPIN_UP_SECONDS = 1;
+  }
+
+  public static final class OperatorConstants {
+    // Port constants for driver and operator controllers. These should match the
+    // values in the Joystick tab of the Driver Station software
     public static final int DRIVER_CONTROLLER_PORT = 0;
     public static final int OPERATOR_CONTROLLER_PORT = 1;
-    public static final int INTAKE_CAMERA_CHANNEL = 0;
-    public static final int SHOOTER_CAMERA_CHANNEL = 1;
 
-    public static final class IntakeConstants {
-        public static final int MOTOR_CHANNEL = 12;
-    }
-
-    public static final class ShooterConstants {
-        public static final int MOTOR_CHANNEL_RIGHT = 10;
-        public static final int MOTOR_CHANNEL_LEFT = 11;
-    }
-    public static final class ArmConstants{
-        public static final int LEFT_MOTOR_CHANNEL = 13;
-        public static final int RIGHT_MOTOR_CHANNEL = 14;
-
-    }
-
-    public static final class InputConstants {
-        public static final double DEADBAND = 0.05;
-    }
-
-    public static final class DriveConstants {
-        public static final int FRONT_LEFT_DRIVING_MOTOR_PORT = 1;
-        public static final int FRONT_RIGHT_DRIVING_MOTOR_PORT = 7;
-        public static final int REAR_LEFT_DRIVING_MOTOR_PORT = 3;
-        public static final int REAR_RIGHT_DRIVING_MOTOR_PORT = 5;
-
-        public static final int FRONT_LEFT_TURNING_MOTOR_PORT = 2;
-        public static final int FRONT_RIGHT_TURNING_MOTOR_PORT = 8;
-        public static final int REAR_LEFT_TURNING_MOTOR_PORT = 4;
-        public static final int REAR_RIGHT_TURNING_MOTOR_PORT = 6;
-        
-        public static final double FRONT_LEFT_ANGULAR_OFFSET = -Math.PI / 2;
-        public static final double FRONT_RIGHT_ANGULAR_OFFSET = 0;
-        public static final double REAR_LEFT_ANGULAR_OFFSET = Math.PI;
-        public static final double REAR_RIGHT_ANGULAR_OFFSET = Math.PI / 2;
-
-        /** Distance between left and right wheels. */
-        public static final double TRACK_WIDTH_METERS = .61;
-        /** Distance between front and back wheels. */
-        public static final double WHEEL_BASE_METERS = .737;
-        public static final SwerveDriveKinematics DRIVE_KINEMATICS =
-            new SwerveDriveKinematics(
-                new Translation2d(WHEEL_BASE_METERS / 2, TRACK_WIDTH_METERS / 2),
-                new Translation2d(WHEEL_BASE_METERS / 2, -TRACK_WIDTH_METERS / 2),
-                new Translation2d(-WHEEL_BASE_METERS / 2, TRACK_WIDTH_METERS / 2),
-                new Translation2d(-WHEEL_BASE_METERS / 2, -TRACK_WIDTH_METERS / 2)   
-            );
-        
-        public static boolean GYRO_REVERSED = false;
-        public static final double SPEED_RATIO = .5;
-        public static final Rotation2d STARTING_ROTATION = new Rotation2d(Math.PI);
-        public static final double DIRECTION_SLEW_RATE = 1.2;
-        public static final double MAGNITUDE_SLEW_RATE = 1.8;
-        public static final double ROTATIONAL_SLEW_RATE = 2.0;
-        public static final double MAX_SPEED_METERS_PER_SECOND = 4.8;
-        public static final double MAX_ANGULAR_SPEED = 2 * Math.PI;
-    }
-
-    public static abstract class SwerveModuleConstants {
-        public static final double DRIVING_P_VALUE = 0.04;
-        public static final double DRIVING_I_VALUE = 0;
-        public static final double DRIVING_D_VALUE = 0;
-
-        public static final double TURNING_P_VALUE = 1;
-        public static final double TURNING_I_VALUE = 0;
-        public static final double TURNING_D_VALUE = 0;
-
-        public static final double WHEEL_DIAMETER_METERS = .0762;
-        public static final double WHEEL_CIRCUMFERENCE_METERS = WHEEL_DIAMETER_METERS * Math.PI;
-
-        // The RevRobotics MAXSwerve Module we're using can be configured with one of three pinion gears: 12T, 13T, or 14T.
-        // Make sure this is accurate to our configuration.
-        // The other 3 teeth numbers are fixed.
-        public static final double DRIVING_MOTOR_PINION_TEETH = 13;
-        public static final double DRIVING_MOTOR_BEVEL_GEAR_TEETH = 45;
-        public static final double DRIVING_MOTOR_SPUR_GEAR_TEETH = 22;
-        public static final double DRIVING_MOTOR_BEVEL_PINION_TEETH = 15;
-        public static final double DRIVING_MOTOR_REDUCTION = (DRIVING_MOTOR_BEVEL_GEAR_TEETH * DRIVING_MOTOR_SPUR_GEAR_TEETH)
-            / (DRIVING_MOTOR_PINION_TEETH * DRIVING_MOTOR_BEVEL_PINION_TEETH);
-        /** Convert the encoder's units into meters. */
-        public static final double DRIVING_ENCODER_POSITION_FACTOR = (WHEEL_DIAMETER_METERS * Math.PI)
-            / DRIVING_MOTOR_REDUCTION;
-
-        public static final double DRIVING_MOTOR_ROTATIONS_PER_SECOND = 5676 / 60;
-        public static final double DRIVING_MOTOR_METERS_PER_SECOND = DRIVING_MOTOR_ROTATIONS_PER_SECOND * WHEEL_CIRCUMFERENCE_METERS
-            / DRIVING_MOTOR_REDUCTION;
-        
-        public static final double DRIVING_FF_VALUE = 1 / DRIVING_MOTOR_METERS_PER_SECOND;
-        public static final double TURNING_FF_VALUE = 0;
-        
-        /** Convert the encoder's units into radians. */
-        public static final double TURNING_ENCODER_POSITION_FACTOR = 2 * Math.PI;
-        public static final boolean TURNING_ENCODER_INVERTED = true;
-        
-        public static final int DRIVING_MOTOR_CURRENT_LIMIT = 30;
-        public static final int TURNING_MOTOR_CURRENT_LIMIT = 20;
-    }
-
-    public static final class AutonomousConstants {
-        public static final double kMaxSpeedMetersPerSecond = 3;
-        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-        public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-        public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
-
-        public static final double kPXController = 1;
-        public static final double kPYController = 1;
-        public static final double kPThetaController = 1;
-
-        // Constraint for the motion profiled robot angle controller
-        public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
-            kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared
-        );
-    }
+    // This value is multiplied by the joystick value when rotating the robot to
+    // help avoid turning too fast and beign difficult to control
+    public static final double DRIVE_SCALING = .7;
+    public static final double ROTATION_SCALING = .8;
+  }
 }
